694b79dfdeb338b3cb01e07304cc818194e2879a
PAZZI14 <pazinatto.tiago@gmail.com>
2025-10-07 20:47:14 -0300

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,13 @@
+// Arquivo: tracking.js
+// Use este arquivo para adicionar seus códigos de rastreamento (Google Analytics, Meta Pixel, etc.)
+
+/*
+// Exemplo de código Google Analytics 4 (GA4)
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-XXXXXX');
+*/
+
+console.log("Tracking: Arquivo pronto para integração de códigos de rastreamento.");
